27b00e520678529dbc83b07d9bac3bad5c83300b
Edumnd <31646030+Edumnd@users.noreply.github.com>
2019-11-22 02:00:35 +1100

    Add files via upload

---
+++ sites/www.qiyuanglobal.com/js/main-about-us.js
@@ -0,0 +1,39 @@
+(function () {
+
+  var bv = new Bideo();
+  bv.init({
+    // Video element
+    videoEl: document.querySelector('#background_video'),
+
+    // Container element
+    container: document.querySelector('body'),
+
+    // Resize
+    resize: true,
+
+    // autoplay: false,
+
+    isMobile: window.matchMedia('(max-width: 768px)').matches,
+
+    playButton: document.querySelector('#play'),
+    pauseButton: document.querySelector('#pause'),
+
+    // Array of objects containing the src and type
+    // of different video formats to add
+    src: [
+      {
+        src: '/sites/www.gtrading.com.au/css/images/web.mp4',
+        type: 'video/mp4'
+      },
+	  {
+       	src: '/sites/www.gtrading.com.au/css/images/web.webm',
+       	type: 'video/webm;codecs="vp8, vorbis"'
+      }
+    ],
+
+    // What to do once video loads (initial frame)
+    onLoad: function () {
+      document.querySelector('#video_cover').style.display = 'none';
+    }
+  });
+}());
